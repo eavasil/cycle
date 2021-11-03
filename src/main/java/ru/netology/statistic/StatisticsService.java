ea770a16b19bff03681349601c7c8e927736a98d
eavasil <dan_kat@mail.ru>
2021-11-03 11:57:41 +0700

    вывела поле, но значение полученное в методе не передается обратно, падает тест на среднее знач.

--- src/main/java/ru/netology/statistic/StatisticsService.java
+++ src/main/java/ru/netology/statistic/StatisticsService.java
@@ -1,27 +1,12 @@
 package ru.netology.statistic;
 
 public class StatisticsService {
+    long sum=0;
+
     public long calculateSum(long[] salesPerMonth) {
-        long sum = 0;
         for (long salesPerMonthes : salesPerMonth) {
             // аналог sum = sum + salesPerMonth;
             sum += salesPerMonthes;
-        }
-        return sum;
-    }
-
-    public int minSales(long[] salesPerMonth) {
-        int minMonth = 0;
-        int month = 0; // переменная для индекса рассматриваемого месяца в массиве
-        for (long salesPerMonthes : salesPerMonth) {
-            // salesPerMonth[minMonth] - продажи в месяце minMonth
-            // salesPerMonthes - продажи в рассматриваемом месяце
-            if (salesPerMonthes <= salesPerMonth[minMonth]) {
-                minMonth = month;
-            }
-            month = month + 1; // следующий рассматриваемый месяц имеет номер на 1 больше
-        }
-        return minMonth + 1;
     }
 
     public int maxSales(long[] salesPerMonth) {
@@ -41,12 +26,10 @@ public class StatisticsService {
     public long findAverage(long[] salesPerMonth) {
         // поиск среднего значения
         long average = 0;
-        long sum1 = 0;
         int month = 0; // переменная для индекса рассматриваемого месяца в массиве
         for (long salesPerMonthes : salesPerMonth) {
-            sum1 += salesPerMonthes;
             month = month + 1;
-            average = sum1 / month;
+            average = sum / month;
         }
         return average;
     }
@@ -71,7 +54,7 @@ public class StatisticsService {
     }
 
     public long moreAverage(long[] salesPerMonth) {
-        // меньше среднего значения
+        // больше среднего значения
         long average = 0;
         long sum1 = 0;
         int month = 0; // переменная для индекса рассматриваемого месяца в массиве
--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -7,19 +7,20 @@ import static org.junit.jupiter.api.Assertions.*;
 public class StatisticsServiceTest {
     @Test
     void calculateSum() {
-        ru.netology.statistic.StatisticsService service = new ru.netology.statistic.StatisticsService();
+        StatisticsService service = new StatisticsService();
 
         long[] salesPerMonth = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         long expected = 180;
 
         long actual = service.calculateSum(salesPerMonth);
+        //сумма всех продаж
 
         assertEquals(expected, actual);
     }
     @Test
     void minSales() {
         // минимальное значение
-        ru.netology.statistic.StatisticsService service = new ru.netology.statistic.StatisticsService();
+        StatisticsService service = new StatisticsService();
         long[] salesPerMonth = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         long expected = 9;
         long actual = service.minSales(salesPerMonth);
@@ -28,7 +29,7 @@ public class StatisticsServiceTest {
     @Test
     void maxSales() {
         // максимальное значение номер месяца
-        ru.netology.statistic.StatisticsService service = new ru.netology.statistic.StatisticsService();
+        StatisticsService service = new StatisticsService();
         long[] salesPerMonth = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         long expected = 8;
         long actual = service.maxSales(salesPerMonth);
@@ -37,7 +38,7 @@ public class StatisticsServiceTest {
     @Test
     void lessAverage() {
         // меньше среднего
-        ru.netology.statistic.StatisticsService service = new ru.netology.statistic.StatisticsService();
+        StatisticsService service = new StatisticsService();
         long[] salesPerMonth = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         long expected = 5;
         long actual = service.lessAverage(salesPerMonth);
@@ -46,7 +47,7 @@ public class StatisticsServiceTest {
     @Test
     void moreAverage() {
         // больше среднего
-        ru.netology.statistic.StatisticsService service = new ru.netology.statistic.StatisticsService();
+        StatisticsService service = new StatisticsService();
         long[] salesPerMonth = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         long expected = 5;
         long actual = service.moreAverage(salesPerMonth);
@@ -55,7 +56,7 @@ public class StatisticsServiceTest {
     @Test
     void findAverage() {
         // среднее значение
-        ru.netology.statistic.StatisticsService service = new ru.netology.statistic.StatisticsService();
+        StatisticsService service = new StatisticsService();
         long[] salesPerMonth = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         long expected = 15;
         long actual = service.findAverage(salesPerMonth);
